36e0ca025c2cf023b23b6570b63b1d6c737d5e84
Ar3Tea <tbusinessr@gmail.com>
2018-08-29 20:34:06 -0400

    build out

---
+++ qa-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <p>Work in progress.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
